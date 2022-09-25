54d5d69b540b47a097b042b00c179209bc6b0a05
Jakob Kofoed Janot <jkj@schultz.dk>
2022-09-25 09:24:22 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IronOcr;
+using System;
+var Ocr = new IronTesseract();
+//var ContentArea = new System.Drawing.Rectangle() { X = 215, Y = 1250, Height = 280, Width = 1335 };
+// Dimensions are in in px
+
+
+//using (var input = new OcrInput("files/example/0038.jpg", ContentArea))
+using (var input = new OcrInput("files/example/0038.jpg"))
+{
+    input.DeNoise();
+    input.Deskew();
+    input.DeepCleanBackgroundNoise();
+    input.EnhanceResolution();
+    input.Contrast();
+    input.ToGrayScale();
+    var result = Ocr.Read(input);
+    Console.WriteLine(result.Text);
+    result.SaveAsHocrFile("results.html");
+}
